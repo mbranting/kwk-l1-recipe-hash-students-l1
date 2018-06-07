131e3e4d4ca9e11d556abbf0f6344be5c9654fb7
mbranting <mckenna.branting@gmail.com>
2018-06-07 16:03:41 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -19,7 +19,12 @@ def recipe_ingredients(perfect_10_recipe) = {
   "gluten freewhole oats" => "1 cup"
   "kosher salt" => "1/2 tea spoon"
   "baking powder" => "1/2 tea spoon"
-  
+  "baking soda" => "1/4 tea spoon"
+  "Xanthan Gum" => "1/2 tea spoon"
+  "slivered almonds, lightly toasted" => "1/4 cup"
+  "mini dark chocolate chips" => "3/4 cup"
+  "olive oil" => "1/2 cup"
+  "agave" => "1/4 cup"
 }
   
 end
