49de8a0a1f49fe2fdb7566db4c93813d690eb278
mbranting <mckenna.branting@gmail.com>
2018-06-07 16:06:39 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -14,7 +14,8 @@
 # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
 
 #2. Write a method that simply returns (not puts) your perfect_10 hash
-def recipe_ingredients(perfect_10_recipe) = {
+def recipe_ingredients(perfect_10_recipe) 
+  perfect_10_recipe = {
   "almond flour" => "2 cups",
   "gluten freewhole oats" => "1 cup",
   "kosher salt" => "1/2 tea spoon",
@@ -26,6 +27,8 @@ def recipe_ingredients(perfect_10_recipe) = {
   "olive oil" => "1/2 cup",
   "agave" => "1/4 cup"
 }
+puts perfect_10_recipe
+end
 return recipe_ingredients
   
 end
